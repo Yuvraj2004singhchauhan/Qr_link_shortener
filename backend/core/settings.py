05eb91717a14baa077d97eca117a275560ef2007
yuvraj2004singhchauhan <yuvrajsinghchauhan2004@gmail.com>
2026-07-20 21:22:41 +0530

    preparing backend for deployment

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -56,6 +56,9 @@ ALLOWED_HOSTS = [
     "127.0.0.1",
     "localhost",
 ]
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
 
 CSRF_TRUSTED_ORIGINS = [
     "https://qrlinkshortener-production.up.railway.app",
