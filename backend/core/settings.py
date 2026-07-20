22b147a215e339fe1fb8556148befad48ef2c30b
yuvraj2004singhchauhan <yuvrajsinghchauhan2004@gmail.com>
2026-07-20 20:05:54 +0530

    preparing backend for deployment

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -17,8 +17,7 @@ load_dotenv(BASE_DIR / ".env")
 SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
